54f3362fe9b05e531c2a192c2a2a1acd3363b884
hux <1184191598@qq.com>
2022-03-04 00:51:02 +0800

    项目初始结构创建

---
+++ xmall-ware/src/main/java/com/hu/xmall/ware/XmallWareApplication.java
@@ -0,0 +1,13 @@
+package com.hu.xmall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallWareApplication.class, args);
+    }
+
+}
