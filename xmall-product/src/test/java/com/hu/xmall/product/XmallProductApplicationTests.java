494172af3574fcc4697546273002a88ae0186594
hux <1184191598@qq.com>
2022-03-10 12:10:11 +0800

    测试pms_brand查询mapper人人开源

--- xmall-product/src/test/java/com/hu/xmall/product/XmallProductApplicationTests.java
+++ xmall-product/src/test/java/com/hu/xmall/product/XmallProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hu.xmall.product;
 
+import com.hu.xmall.product.entity.BrandEntity;
+import com.hu.xmall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XmallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandEntity.setDescript("ss");
+        brandService.save(brandEntity);
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("华为");
+//        brandService.removeById(brandEntity);
+        System.out.println("aa");
     }
 
 }
